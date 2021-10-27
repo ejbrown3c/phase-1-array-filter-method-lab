f1f457e3332a3c0779f60abf080d6e3ad333f39d
EJ Brown <ejbrown3c@gmail.com>
2021-10-27 14:40:53 -0400

    commit tree 446bbb1064ca5d56531f30570fce492bfa2fb865

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+
+
+function findMatching (drivers, string) {
+    const filterDrivers = drivers.filter(dN => dN.toLowerCase() === string.toLowerCase());
+    return filterDrivers;
+} 
+ 
+findMatching ('Bobby')
+
+function fuzzyMatch (drivers, string) {
+  const matchDrivers = drivers.filter(driversMatch => driversMatch.substring(0,2) === string );
+    return matchDrivers;
+}
+
+function matchName (drivers, string) {
+    return drivers.filter(function(hometown){ 
+    return hometown.name === string })
+}
